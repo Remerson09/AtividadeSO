db1ed01e16735813ea9a4928ce3de9c157faa4bc
Ian <ian@gmail.com>
2023-12-05 11:16:50 -0300

    Atualização do codigo

--- src/main/java/com/example/demo/controller/ProdutoController.java
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.ProdutoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,21 +18,13 @@ public class ProdutoController {
     // Injeta (cria automaticamente uma instância do tipo da variável
     private ProdutoRepository repository;
 
-
-
-    /* Indica que o métado deve ser chamado enviando requisição HTTP GET.
-    * Só após isso é que o método fica disponível para acessar  via HTTP.
-    */
-
     @GetMapping
     public List<Produto>findAll(){
+
         return repository.findAll();
     }
     
-    @PostMapping
-    public Produto insert( @RequestBody Produto produto){
-        return repository.save(produto);
-    }
+
     @GetMapping("/{id}")
     public Produto findById( @PathVariable long id){
         return repository.
@@ -39,5 +32,28 @@ public class ProdutoController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @DeleteMapping("/{id}")
+    public void deletar( @PathVariable long id){
+        //repository.deleteById(id);
+        repository.findById(id)
+                .stream()
+                .peek(produto -> repository.delete(produto))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException( HttpStatus.NOT_FOUND));
+    }
+    @PostMapping
+    public Produto insert( @RequestBody Produto produto){
+        return repository.save(produto);
+    }
+    /* Indica que o métado deve ser chamado enviando requisição HTTP GET.
+     * Só após isso é que o método fica disponível para acessar  via HTTP.
+     */
+    @PutMapping("{id}")
+    public Produto update(@PathVariable long id, @RequestBody Produto produto){
+       final var msg = "O ID informado não conincide com o ID do objeto passado";
+        if (id != produto.getId())
+            throw new ResponseStatusException(HttpStatus.CONFLICT, msg);
 
+        return repository.save(produto);
+    }
 }
